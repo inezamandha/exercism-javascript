2462af4291f78bd61433e5ec660938a3244ac8e6
Inez Amandha <inezamandhasuci@gmail.com>
2024-10-22 21:01:28 +0700

    ✨ Add "Lucky Numbers"

---
+++ lucky-numbers/lucky-numbers.js
@@ -0,0 +1,40 @@
+// @ts-check
+
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+export function twoSum(array1, array2) {
+  let total = Number(array1.join("")) + Number(array2.join(""));
+  return total;
+}
+
+/**
+ * Checks whether a number is a palindrome.
+ *
+ * @param {number} value
+ * @returns {boolean} whether the number is a palindrome or not
+ */
+export function luckyNumber(value) {
+  return value.toString().split("").reverse().join("") === value.toString();
+}
+
+/**
+ * Determines the error message that should be shown to the user
+ * for the given input value.
+ *
+ * @param {string|null|undefined} input
+ * @returns {string} error message
+ */
+export function errorMessage(input) {
+  let number = Number(input);
+  if (input && number) {
+    return "";
+  } else if (input && !number) {
+    return "Must be a number besides 0";
+  }
+  return "Required field";
+}
