df11b58e097c6d2051ef46f787ddf87ef46ea3ae
Inez Amandha <inezamandhasuci@gmail.com>
2024-10-30 11:27:15 +0700

    ✨ Add "Leap"

---
+++ leap/leap.js
@@ -0,0 +1,13 @@
+//
+// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+/**
+ *
+ * @param {number} year
+ */
+
+export const isLeap = (year) => {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+};
